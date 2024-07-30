353b2d0ad2c858c693abc80c294305f37727bb93
Darsh Kejriwal <c-darsh@aulacube.com>
2024-07-30 12:43:21 +0530

    fix: script

--- src/types.ts
+++ src/types.ts
@@ -1,43 +1,41 @@
-import { StringLike } from "bun";
+// import { StringLike } from "bun";
 
-type Classroom {
-	id: string;
-	owner: userId;
-	name: string;
-	questionBanks: QuestionBank[];
-	quizHistory: Quiz[];
-	activeQuiz: Quiz;
-	participants: userId[];
-}
+// type Classroom {
+// 	id: string;
+// 	owner: userId;
+// 	name: string;
+// 	questionBanks: QuestionBank[];
+// 	quizHistory: Quiz[];
+// 	activeQuiz: Quiz;
+// 	participants: userId[];
+// }
 
+// type QuestionBank {
+// 	id: string;
+// 	topic: string;
+// 	ownerId: string;
+// 	questions: Question[]; // 5 question
+// }
 
-type QuestionBank {
-	id: string;
-	topic: string;
-	ownerId: string;
-	questions: Question[]; // 5 question
-}
+// type Question {
+// 	id: string;
+// 	question: string;
+// 	image: string;
+// 	options: string[];
+// 	answer: number; // Option index
+// 	explanation: string;
+// }
 
-type Question {
-	id: string;
-	question: string;
-	image: string;
-	options: string[];
-	answer: number; // Option index
-	explanation: string;
-}
+// type Quiz {
+// 	id: string;
+// 	question: Question[];
+// 	participants: userId[];
+// 	owner: userId;
+// 	isActive: boolean;
+// 	result: QuizResult[];
+// }
 
-type Quiz {
-	id: string;
-	question: Question[];
-	participants: userId[];
-	owner: userId;
-	isActive: boolean;
-	result: QuizResult[];
-}
-
-
-type QuizResult {
-	userId: string;
-	score: number;
-}
+// type QuizResult {
+// 	userId: string;
+// 	score: number;
+// }
