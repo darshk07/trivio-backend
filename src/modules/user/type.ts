f09ada3941cc98ed58d474fe2216d6837acdb581
Darsh Kejriwal <c-darsh@aulacube.com>
2024-07-30 12:47:29 +0530

    update: package-lock

--- src/modules/user/type.ts
+++ src/modules/user/type.ts
@@ -1,7 +1,9 @@
 export type User = {
   id: string;
+  name: string;
   email: string;
-  password: string;
+  role: string;
+  interests: string;
   created_at?: string;
   updated_at?: string;
 };
